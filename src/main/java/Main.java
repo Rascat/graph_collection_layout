eaef9c742caa1f7608f7f5e348727c6e5b4bae99
Lulu Roth <rotkohl.ente@riseup.net>
2019-11-27 16:46:23 +0100

    Add ldbc to gradoop data converter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,11 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.LocalCollectionOutputFormat;
 import org.gradoop.flink.io.api.DataSink;
 import org.gradoop.flink.io.impl.csv.CSVDataSink;
+import org.gradoop.flink.io.impl.edgelist.functions.CreateImportVertex;
+import org.gradoop.flink.io.impl.graph.GraphDataSource;
+import org.gradoop.flink.io.impl.graph.tuples.ImportVertex;
 import org.gradoop.flink.model.impl.epgm.GraphCollectionFactory;
+import org.gradoop.flink.model.impl.epgm.LogicalGraphFactory;
 import org.gradoop.flink.util.GradoopFlinkConfig;
 import org.s1ck.ldbc.LDBCToFlink;
 import org.s1ck.ldbc.tuples.LDBCEdge;
@@ -24,16 +28,10 @@ public class Main {
         DataSet<LDBCEdge> edges = ldbcToFlink.getEdges();
         DataSet<LDBCVertex> vertices = ldbcToFlink.getVertices();
 
-        List<LDBCVertex> vertexList = new ArrayList<>();
-        List<LDBCEdge> edgeList = new ArrayList<>();
-
-        edges.output(new LocalCollectionOutputFormat<>(edgeList));
-        vertices.output(new LocalCollectionOutputFormat<>(vertexList));
 
         GraphCollectionFactory factory = new GraphCollectionFactory(cfg);
         DataSink sink = new CSVDataSink("out/", cfg);
 
         env.execute();
-        System.out.println(vertexList);
     }
 }
