bc198ab4290c6cb5671ea6032fef6eee896d3466
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-12-13 15:09:55 +0100

    Modify ForceDirectedGraphCollectionLayout

--- src/main/java/org/rascat/gcl/layout/ForceDirectedGraphCollectionLayout.java
+++ src/main/java/org/rascat/gcl/layout/ForceDirectedGraphCollectionLayout.java
@@ -1,17 +1,22 @@
 package org.rascat.gcl.layout;
 
 import org.apache.flink.api.java.DataSet;
+import org.gradoop.common.model.api.entities.GraphHead;
 import org.gradoop.common.model.impl.pojo.EPGMEdge;
+import org.gradoop.common.model.impl.pojo.EPGMGraphHead;
 import org.gradoop.common.model.impl.pojo.EPGMVertex;
 import org.gradoop.flink.model.impl.epgm.GraphCollection;
+import org.rascat.gcl.functions.ComputeRepulsiveForces;
 import org.rascat.gcl.functions.RandomPlacement;
+import org.rascat.gcl.functions.SumForces;
+import org.rascat.gcl.model.Force;
 
 public class ForceDirectedGraphCollectionLayout {
 
     private int width;
     private int height;
     private double k;
-    private int iterations = 10;
+    private int iterations = 1;
 
     public ForceDirectedGraphCollectionLayout(int width, int height) {
         this.height = height;
@@ -26,14 +31,19 @@ public class ForceDirectedGraphCollectionLayout {
         this.k = Math.sqrt((double) area() / vertexCount);
         DataSet<EPGMEdge> edges = collection.getEdges();
         DataSet<EPGMVertex> vertices = collection.getVertices();
+        DataSet<EPGMGraphHead> graphHeads = collection.getGraphHeads();
 
         vertices = vertices.map(new RandomPlacement(width, height));
 
-        for (int i = 0; i < iterations; i++) {
-            vertices.
-        }
+        DataSet<Force> displacements =
+          vertices.cross(vertices).with(new ComputeRepulsiveForces(k));
 
-        return null;
+        DataSet<Force> displacementByVertex = displacements.groupBy("f0").reduce(new SumForces());
+
+        displacements.writeAsText("out/displacements");
+        displacementByVertex.writeAsText("out/dispByVertex");
+
+        return collection.getFactory().fromDataSets(graphHeads, vertices, edges);
     }
 
     private int area() {
