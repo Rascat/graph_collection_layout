d39aaf5536ff941a37c627d246c08db8c133bef2
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-12-13 15:09:05 +0100

    Add function SumForces

--- src/main/java/org/rascat/gcl/functions/SumForces.java
+++ src/main/java/org/rascat/gcl/functions/SumForces.java
@@ -1,4 +1,13 @@
 package org.rascat.gcl.functions;
 
-public class SumForces {
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.rascat.gcl.model.Force;
+
+public class SumForces implements ReduceFunction<Force> {
+
+  @Override
+  public Force reduce(Force force, Force t1) throws Exception {
+
+    return new Force(force.getId(), force.getVector().add(t1.getVector()));
+  }
 }
