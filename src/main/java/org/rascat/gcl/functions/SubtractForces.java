ca6d30cc9bb0852a7be8b24bd6515af318da2f9e
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-12-18 15:00:14 +0100

    Add reduce function SubtractForces

---
+++ src/main/java/org/rascat/gcl/functions/SubtractForces.java
@@ -0,0 +1,11 @@
+package org.rascat.gcl.functions;
+
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.rascat.gcl.model.Force;
+
+public class SubtractForces implements ReduceFunction<Force> {
+    @Override
+    public Force reduce(Force force, Force t1) throws Exception {
+        return new Force(force.getId(), force.getVector().subtract(t1.getVector()));
+    }
+}
