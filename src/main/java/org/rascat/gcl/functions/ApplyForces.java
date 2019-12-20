b8671d55a7f99aa5c1e4a6d7c922d75ade252ab2
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-12-20 13:47:56 +0100

    Refactor class ApplyForces to use sim annealing

--- src/main/java/org/rascat/gcl/functions/ApplyForces.java
+++ src/main/java/org/rascat/gcl/functions/ApplyForces.java
@@ -4,47 +4,49 @@ import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.RichJoinFunction;
 import org.gradoop.common.model.impl.pojo.EPGMVertex;
+import org.rascat.gcl.layout.AbstractGraphCollectionLayout;
 import org.rascat.gcl.model.Force;
+
+import static org.rascat.gcl.layout.AbstractGraphCollectionLayout.*;
 
 public class ApplyForces extends RichJoinFunction<EPGMVertex, Force, EPGMVertex> {
 
-    private double t;
     private int width;
     private int height;
 
-    public ApplyForces(int width, int height) {
-        this((double) width / 10, width, height);
-    }
+    private CoolingSchedule schedule;
 
-    public ApplyForces(double t, int width, int height) {
-        this.t = t;
+    public ApplyForces(int width, int height, CoolingSchedule schedule) {
         this.width = width;
         this.height = height;
+        this.schedule = schedule;
+    }
+
+    public void setSchedule(CoolingSchedule schedule) {
+        this.schedule = schedule;
     }
 
     @Override
     public EPGMVertex join(EPGMVertex vertex, Force force) throws Exception {
-        double x = vertex.getPropertyValue("X").getDouble();
-        double y = vertex.getPropertyValue("Y").getDouble();
+        double x = vertex.getPropertyValue(KEY_X_COORD).getDouble();
+        double y = vertex.getPropertyValue(KEY_Y_COORD).getDouble();
 
         Vector2D vPos = new Vector2D(x, y);
         Vector2D vDisp = force.getVector();
 
-        vPos = vPos.add(vDisp.scalarMultiply(1 / vDisp.getNorm()).scalarMultiply(Math.min(vDisp.getNorm(), t)));
+
+        double temperature = schedule.computeTemperature(getIterationRuntimeContext().getSuperstepNumber());
+        vPos = vPos.add(vDisp.scalarMultiply(1 / vDisp.getNorm()).scalarMultiply(Math.min(vDisp.getNorm(), temperature)));
         double vPosX = vPos.getX();
         double vPosY = vPos.getY();
 
         vPosX = Math.min(width, Math.max(0, vPosX));
         vPosY = Math.min(height, Math.max(0, vPosY));
 
-        vertex.setProperty("X", vPosX);
-        vertex.setProperty("Y", vPosY);
+        vertex.setProperty(KEY_X_COORD, vPosX);
+        vertex.setProperty(KEY_Y_COORD, vPosY);
 
         System.out.println(String.format("[%s]: (%.0f, %.0f)-->(%.0f, %.0f)", vertex.getId().toString().substring(vertex.getId().toString().length() - 4), x, y, vPosX, vPosY));
         return vertex;
-    }
-
-    private void cool() {
-
     }
 }
