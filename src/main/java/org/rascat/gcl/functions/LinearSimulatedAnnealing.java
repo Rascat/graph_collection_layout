a7b4bd9598df175c450a0bc72f4dcfcf0d1e8e8a
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-12-20 13:47:08 +0100

    add class LinearSimulatedAnnealing

--- src/main/java/org/rascat/gcl/functions/LinearSimulatedAnnealing.java
+++ src/main/java/org/rascat/gcl/functions/LinearSimulatedAnnealing.java
@@ -1,4 +1,15 @@
 package org.rascat.gcl.functions;
 
-public class LinearSimulatedAnnealing {
+public class LinearSimulatedAnnealing implements CoolingSchedule {
+
+  private double startTemperature;
+
+  public LinearSimulatedAnnealing(double startTemperature) {
+    this.startTemperature = startTemperature;
+  }
+
+  @Override
+  public double computeTemperature(int currentIteration) {
+    return startTemperature / currentIteration;
+  }
 }
