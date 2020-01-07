f6f21fc2ad4d45957dd04eda2c3c27e63fa15678
Lucas Schons <schons@studserv.uni-leipzig.de>
2020-01-07 13:42:26 +0100

    Add setters to class Force

--- src/main/java/org/rascat/gcl/model/Force.java
+++ src/main/java/org/rascat/gcl/model/Force.java
@@ -6,6 +6,9 @@ import org.gradoop.common.model.impl.id.GradoopId;
 
 public class Force extends Tuple2<GradoopId, Vector2D> {
 
+  public final static String ID_POSITION = "f0";
+  public final static String VECTOR_POSITION = "f1";
+
   public Force(GradoopId id, Vector2D vector) {
     super(id, vector);
   }
@@ -20,6 +23,14 @@ public class Force extends Tuple2<GradoopId, Vector2D> {
 
   public Vector2D getVector() {
     return this.f1;
+  }
+
+  public void setId(GradoopId id) {
+    this.f0 = id;
+  }
+
+  public void setVector(Vector2D vector) {
+    this.f1 = vector;
   }
 
   @Override
