e9652901b093595634d0af5e47369289afec5c88
Lucas Schons <schons@studserv.uni-leipzig.de>
2020-01-08 11:50:43 +0100

    refactor force-calc-py

--- bin/force-calc.py
+++ bin/force-calc.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python3
 # FR force calculator for two vertices connected by an edge
+import argparse
 import math
 
-
 def main():
-    w = int(input('width: '))
-    h = int(input('height: '))
-    v = tuple([int(x) for x in input('v (x y): ').split()])
-    u = tuple([int(x) for x in input('u (x y): ').split()])
+    parser = argparse.ArgumentParser(description='Compute displacement of two vertices connected by an edge')
+    parser.add_argument('v', type=str, help='position of vertex v (x, y)')
+    parser.add_argument('u', type=str, help='position of vertex u (x, y)')
+    parser.add_argument('width', type=int, help='width of the layout space')
+    parser.add_argument('height', type=int, help='height of the layout space')
+    parser.add_argument('-t', type=float, help='cooling factor t - defaults to 1.0', default=1)
+
+    args = parser.parse_args()
+
+    w = args.width
+    h = args.height
+    t = args.t
+
+    v = tuple([int(x) for x in args.v.split(',')])
+    u = tuple([int(x) for x in args.u.split(',')])
 
     area = w * h
     k = math.sqrt(area / 2)
@@ -29,17 +41,28 @@ def main():
     # apply forces
     v_disp_len = math.sqrt((v_disp[0] * v_disp[0]) + (v_disp[1] * v_disp[1]))
     v_disp_norm = (v_disp[0] / v_disp_len, v_disp[1] / v_disp_len)
+    u_disp_len = math.sqrt((u_disp[0] * u_disp[0]) + (u_disp[1] * u_disp[1]))
+    u_disp_norm = (u_disp[0] / u_disp_len, u_disp[1] / u_disp_len)
     
-    # ignore min(v.disp, t) part -> might need to adapt later!
-    v_new = (v[0] + v_disp[0], v[1] + v_disp[1])
-    u_new = (u[0] + u_disp[0], u[1] + u_disp[1])
+    v_new_x = int(v[0] + v_disp_norm[0] * t)
+    v_new_y = int(v[1] + v_disp_norm[1] * t)
+    v_new_x = min(w, max(0, v_new_x))
+    v_new_y = min(h, max(0, v_new_y))
 
-    out = '\n\nv: {}\nu: {}'.format(v_new, u_new)
+    u_new_x = int(u[0] + u_disp_norm[0] * t)
+    u_new_y = int(u[1] + u_disp_norm[1] * t)
+    u_new_x = min(w, max(0, u_new_x))
+    u_new_y = min(h, max(0, u_new_y))
+    
+    v_new = (v_new_x, v_new_y)
+    u_new = (u_new_x, u_new_y)
+
+    out = 'v: {}\nu: {}'.format(v_new, u_new)
 
     print(out)
     print('\nk (optimal distance): {}'.format(k))
     print('\nold distance: {}'.format(distance(v, u)))
-    print('\nnew distance: {}'.format(distance(v_new, u_new)))
+    print('new distance: {}'.format(distance(v_new, u_new)))
 
 
 def distance(v, u):
