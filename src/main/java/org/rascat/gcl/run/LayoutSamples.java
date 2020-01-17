6ca47cf43ec204303c81369c3b3afe9d21b7e71b
Lucas Schons <schons@studserv.uni-leipzig.de>
2020-01-17 13:28:14 +0100

    Setup sampling env with supported layout algorithms

--- src/main/java/org/rascat/gcl/run/LayoutSamples.java
+++ src/main/java/org/rascat/gcl/run/LayoutSamples.java
@@ -1,13 +1,11 @@
 package org.rascat.gcl.run;
 
-import org.apache.commons.cli.ParseException;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.gradoop.flink.io.impl.image.ImageDataSink;
 import org.gradoop.flink.model.impl.epgm.GraphCollection;
-import org.gradoop.flink.model.impl.operators.layouting.LayoutingAlgorithm;
+import org.gradoop.flink.model.impl.operators.layouting.*;
 import org.gradoop.flink.util.FlinkAsciiGraphLoader;
 import org.gradoop.flink.util.GradoopFlinkConfig;
-import org.gradoop.flink.model.impl.operators.layouting.FRLayouter;
 
 import java.io.File;
 import java.text.DateFormat;
@@ -15,38 +13,52 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class LayoutSamples {
-    public static void main(String[] args) throws Exception {
-        LayoutParameters params = new LayoutParameters(args);
-        int height = params.height(1000);
-        int width = params.width(1000);
-        int iterations = params.iteration(1);
-        int vertices = params.vertices(20);
+  public static void main(String[] args) throws Exception {
+    LayoutParameters params = new LayoutParameters(args);
+    int height = params.height(1000);
+    int width = params.width(1000);
+    int iterations = params.iteration(1);
+    int vertices = params.vertices(20);
 
-        ExecutionEnvironment env = ExecutionEnvironment.createLocalEnvironment();
-        GradoopFlinkConfig cfg = GradoopFlinkConfig.createConfig(env);
+    ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+    GradoopFlinkConfig cfg = GradoopFlinkConfig.createConfig(env);
 
-        FlinkAsciiGraphLoader loader = new FlinkAsciiGraphLoader(cfg);
-        loader.initDatabaseFromFile(params.inputPath());
+    FlinkAsciiGraphLoader loader = new FlinkAsciiGraphLoader(cfg);
+    loader.initDatabaseFromFile(params.inputPath());
 
-        GraphCollection collection = loader.getGraphCollection();
+    GraphCollection collection = loader.getGraphCollection();
 
-        for (int i = 1; i < 10; i++) {
-            FRLayouter layout = new FRLayouter(i, vertices);
-            layout.area(width, height);
+    FRLayouter frLayout = new FRLayouter(iterations, vertices);
+    CombiLayouter combiLayout = new CombiLayouter(iterations, vertices, .5D);
+    FusingFRLayouter fusingLayout = new FusingFRLayouter(iterations, vertices, 0.7D, FusingFRLayouter.OutputFormat.EXTRACTED);
+    CentroidFRLayouter centroidFRLayout = new CentroidFRLayouter(iterations, vertices);
 
-            collection = layout.execute(collection);
+    GraphCollection frCollection = frLayout.execute(collection);
+    GraphCollection combiCollection = combiLayout.execute(collection);
+    GraphCollection fusingCollection = fusingLayout.execute(collection);
+    GraphCollection centroidCollection = centroidFRLayout.execute(collection);
 
-            String out = params.outputPath() + File.separator + createImgFileName(layout, i);
-            ImageDataSink sink = new ImageDataSink(out, layout, width, height);
-            collection.writeTo(sink);
-        }
+    String frPath = params.outputPath() + File.separator + createImgFileName(frLayout, iterations);
+    String combiPath = params.outputPath() + File.separator + createImgFileName(combiLayout, iterations);
+    String fusingPath = params.outputPath() + File.separator + createImgFileName(fusingLayout, iterations);
+    String centroidPath = params.outputPath() + File.separator + createImgFileName(centroidFRLayout, iterations);
 
-        env.execute();
-    }
+    ImageDataSink frSink = new ImageDataSink(frPath, frLayout, width, height);
+    ImageDataSink combiSink = new ImageDataSink(combiPath, combiLayout, width, height);
+    ImageDataSink fusingSink = new ImageDataSink(fusingPath, fusingLayout, width, height);
+    ImageDataSink centroidSink = new ImageDataSink(centroidPath, centroidFRLayout, width, height);
 
-    private static String createImgFileName(LayoutingAlgorithm algorithm, int iterations) {
-        DateFormat dateFormat = new SimpleDateFormat("yyMMdd_HH:mm:ss");
-        Date date = new Date();
-        return String.format("%s_%s_%d.png", dateFormat.format(date), algorithm.getName(), iterations);
-    }
+    frCollection.writeTo(frSink);
+    combiCollection.writeTo(combiSink);
+    fusingCollection.writeTo(fusingSink);
+    centroidCollection.writeTo(centroidSink);
+
+    env.execute();
+  }
+
+  private static String createImgFileName(LayoutingAlgorithm algorithm, int iterations) {
+    DateFormat dateFormat = new SimpleDateFormat("yyMMdd_HH:mm:ss");
+    Date date = new Date();
+    return String.format("%s_%s_%d.png", dateFormat.format(date), algorithm.getName(), iterations);
+  }
 }
