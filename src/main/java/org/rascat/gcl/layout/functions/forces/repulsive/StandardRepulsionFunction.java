00a8cf81c707fe7ad80f4f332f04b1ca5de9736b
Lucas Schons <schons@studserv.uni-leipzig.de>
2020-02-28 16:37:32 +0100

    Add simple relocation function
    StandardRepulstionFunction now supports simple relocation of a vertex if
    two vertices occupy the same space.

--- src/main/java/org/rascat/gcl/layout/functions/forces/repulsive/StandardRepulsionFunction.java
+++ src/main/java/org/rascat/gcl/layout/functions/forces/repulsive/StandardRepulsionFunction.java
@@ -6,7 +6,12 @@ import org.apache.flink.api.common.functions.FlatJoinFunction;
 import org.apache.flink.util.Collector;
 import org.gradoop.common.model.impl.id.GradoopId;
 import org.gradoop.common.model.impl.pojo.EPGMVertex;
+import org.rascat.gcl.layout.AbstractGraphCollectionLayout;
 import org.rascat.gcl.layout.model.Force;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.rascat.gcl.layout.AbstractGraphCollectionLayout.*;
 
 public class StandardRepulsionFunction extends RepulsionFunction implements FlatJoinFunction<EPGMVertex, EPGMVertex, Force> {
 
@@ -30,8 +35,10 @@ public class StandardRepulsionFunction extends RepulsionFunction implements Flat
     }
 
     if (distance == 0) {
-      // @TODO separate the two vertices by placing one of the nearby, at a random location
+      relocate(v);
+      setPositionalValues(v, u);
     }
+
     return getForce(v.getId());
   }
 
@@ -44,8 +51,10 @@ public class StandardRepulsionFunction extends RepulsionFunction implements Flat
     }
 
     if (distance == 0) {
-      // @TODO separate the two vertices by placing one of the nearby, at a random location
+      relocate(v);
+      setPositionalValues(v, u);
     }
+
     out.collect(getForce(v.getId()));
   }
 
@@ -63,5 +72,37 @@ public class StandardRepulsionFunction extends RepulsionFunction implements Flat
 
   private double repulsion(double distance, double optimalDistance) {
     return (optimalDistance * optimalDistance) / distance;
+  }
+
+  /**
+   * Generate a random integer between 0 and 7 and move the vertex in a direction as illustrated below.
+   *
+   *    7  0  1
+   *     \ | /
+   *      \|/
+   * 6 –––( )––– 2
+   *      /|\
+   *     / | \
+   *    5  4  3
+   *
+   * @param  vertex The vertex to be relocated.
+   */
+  private void relocate(EPGMVertex vertex) {
+    int direction = ThreadLocalRandom.current().nextInt(8);
+    int x = (int) vertex.getPropertyValue(KEY_X_COORD).getDouble();
+    int y = (int) vertex.getPropertyValue(KEY_Y_COORD).getDouble();
+
+    switch(direction) {
+      case 0: y -= 1; break;
+      case 1: x += 1; y -= 1; break;
+      case 2: y += 1; break;
+      case 3: x += 1; y += 1; break;
+      case 4: x += 1; break;
+      case 5: x -= 1; y += 1; break;
+      case 6: x -= 1; break;
+      case 7: x -= 1; y -= 1; break;
+    }
+    vertex.setProperty(KEY_X_COORD, (double) x);
+    vertex.setProperty(KEY_Y_COORD, (double) y);
   }
 }
