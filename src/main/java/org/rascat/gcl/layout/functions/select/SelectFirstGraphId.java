97ae61a1da0b8852254fc22b85aedb0e13d8aa82
Lucas Schons <schons@studserv.uni-leipzig.de>
2020-02-28 12:27:21 +0100

    Modify SelectFirstGraphId
    Convert GradoopIdSet to sorted set and then return first element

--- src/main/java/org/rascat/gcl/layout/functions/select/SelectFirstGraphId.java
+++ src/main/java/org/rascat/gcl/layout/functions/select/SelectFirstGraphId.java
@@ -4,9 +4,13 @@ import org.apache.flink.api.java.functions.KeySelector;
 import org.gradoop.common.model.api.entities.GraphElement;
 import org.gradoop.common.model.impl.id.GradoopId;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class SelectFirstGraphId<E extends GraphElement> implements KeySelector<E, GradoopId> {
   @Override
   public GradoopId getKey(E element) {
-    return element.getGraphIds().iterator().next();
+    SortedSet<GradoopId> sortedIdSet = new TreeSet<>(element.getGraphIds());
+    return sortedIdSet.first();
   }
 }
